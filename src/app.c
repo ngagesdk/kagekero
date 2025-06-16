227521b7eeca5d892e62f1a3565b1202a04cef7d
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-06-16 08:18:33 +0200

    Add dynamic window size on Windows.

--- src/app.c
+++ src/app.c
@@ -33,12 +33,49 @@ bool init_app(SDL_Renderer **renderer, SDL_Window *window)
         SDL_Log("Couldn't initialize gamepad subsystem: %s", SDL_GetError());
     }
 
-    window = SDL_CreateWindow("ncore", WINDOW_W * SCALE, WINDOW_H * SCALE, WINDOW_FLAGS);
+#ifdef WIN32
+    SDL_DisplayID display_id = SDL_GetPrimaryDisplay();
+    if (!display_id)
+    {
+        SDL_Log("Couldn't get primary display: %s", SDL_GetError());
+        return false;
+    }
+
+    SDL_Rect display_bounds;
+    if (!SDL_GetDisplayBounds(display_id, &display_bounds))
+    {
+        SDL_Log("Couldn't get display bounds: %s", SDL_GetError());
+        return false;
+    }
+
+    int max_scale = SDL_min(display_bounds.w / WINDOW_W, display_bounds.h / WINDOW_H);
+    if (max_scale < 1)
+    {
+        max_scale = 1;
+    }
+
+    int window_w = WINDOW_W * max_scale;
+    int window_h = WINDOW_H * max_scale;
+
+    int window_x = display_bounds.x + (display_bounds.w - window_w) / 2;
+    int window_y = display_bounds.y + (display_bounds.h - window_h) / 2;
+#else
+    int window_w = WINDOW_W * SCALE;
+    int window_h = WINDOW_H * SCALE;
+    int max_scale = SCALE;
+#endif
+
+    window = SDL_CreateWindow("ncore", window_w, window_h, WINDOW_FLAGS);
     if (!window)
     {
         SDL_Log("Couldn't create window: %s", SDL_GetError());
         return false;
     }
+
+#if WIN32
+    SDL_WINDOWPOS_CENTERED_DISPLAY(display_id);
+    // SDL_SetWindowFullscreen(window, true);
+#endif
 
     *renderer = SDL_CreateRenderer(window, 0);
     if (!*renderer)
@@ -47,18 +84,15 @@ bool init_app(SDL_Renderer **renderer, SDL_Window *window)
         return false;
     }
 
-#ifdef WIN32
-    // Todo: add proper error handling.
-    SDL_DisplayID id = SDL_GetDisplays(NULL);
-    SDL_DisplayMode **mode = SDL_GetFullscreenDisplayModes(id, NULL);
-    SDL_SetWindowFullscreenMode(window, &mode[0]);
-    SDL_SetWindowFullscreen(window, true);
-#endif
-
-    if (!SDL_SetRenderScale(*renderer, SCALE, SCALE))
+    if (!SDL_SetRenderScale(*renderer, max_scale, max_scale))
     {
         SDL_Log("Could not apply drawing scale factor: %s", SDL_GetError());
         return false;
+    }
+
+    if (!SDL_SetTextureScaleMode(*renderer, SDL_SCALEMODE_NEAREST))
+    {
+        SDL_Log("Couldn't set scale mode: %s", SDL_GetError());
     }
 
     if (!SDL_DisableScreenSaver())
