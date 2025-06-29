c81d03ccc63c294e0368098e34a464e6bf6322b8
Michael Fitzmayer <mail@michael-fitzmayer.de>
2025-06-30 00:28:23 +0200

    Add overlay, not yet implemented.

---
+++ src/overlay.c
@@ -0,0 +1,13 @@
+/** @file overlay.c
+ *
+ *  A minimalist, cross-platform puzzle-platformer, designed
+ *  especially for the Nokia N-Gage.
+ *
+ *  Copyright (c) 2025, Michael Fitzmayer. All rights reserved.
+ *  SPDX-License-Identifier: MIT
+ *
+ **/
+
+#include <SDL3/SDL.h>
+
+#include "overlay.h"
---
+++ src/overlay.h
@@ -0,0 +1,16 @@
+/** @file overlay.h
+ *
+ *  A minimalist, cross-platform puzzle-platformer, designed
+ *  especially for the Nokia N-Gage.
+ *
+ *  Copyright (c) 2025, Michael Fitzmayer. All rights reserved.
+ *  SPDX-License-Identifier: MIT
+ *
+ **/
+
+#ifndef OVERLAY_H
+#define OVERLAY_H
+
+#include <SDL3/SDL.h>
+
+#endif // OVERLAY_H
