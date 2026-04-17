7d0a7cbca327867f707704e0149d45e45b4c9def
Michael Fitzmayer <mail@michael-fitzmayer.de>
2026-04-17 11:55:55 +0200

    Update name of zlib static library which was set incorrectly

--- cmake/get_zlib.cmake
+++ cmake/get_zlib.cmake
@@ -10,9 +10,10 @@ macro(get_zlib version)
       SHA256=bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16
   )
   set(ZLIB_BUILD_EXAMPLES OFF CACHE BOOL "Disable Zlib Examples" FORCE)
-  set(ZLIB_BUILD_SHARED OFF CACHE BOOL "Disable Zlib Shared Library" FORCE)
+  set(ZLIB_BUILD_STATIC ON CACHE BOOL "Enable building zlib static library" FORCE)
+  set(ZLIB_BUILD_SHARED OFF CACHE BOOL "Disable building zlib shared library" FORCE)
 
   FetchContent_MakeAvailable(zlib)
 
-  set(ZLIB_LIBRARIES zlib)
+  set(ZLIB_LIBRARIES zlibstatic)
 endmacro(get_zlib)
