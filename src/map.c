c891ae0039159db07058c9becbd30d678c40ebb5
Michael Fitzmayer <mail@michael-fitzmayer.de>
2026-04-20 10:21:03 +0200

    Minor cold-path cleanup in map.c (possibly speeding up map-loading)
    - Replace tile_index counter with direct row_base + index_width indexing in load_tiles and render_map static tile loop
    - Consolidate 4x repeated property linear scans into a single pass over the properties array in load_tiles
    - Unroll static tile rendering inner loop 4x with remainder
    - Cache map->handle, tilewidth, tileheight and map_width locals in the object group rendering section to reduce pointer chasing

--- src/map.c
+++ src/map.c
@@ -541,34 +541,40 @@ static bool load_tiles(map_t *map)
         if (is_layer_of_type(TILE_LAYER, layer, map))
         {
             int *layer_content = layer->data;
-            register int tile_index = 0;
-
             for (int index_height = 0; index_height < map_height; index_height += 1)
             {
-                for (int index_width = 0; index_width < map_width; index_width += 1, tile_index += 1)
+                register int row_base = index_height * map_width;
+                for (int index_width = 0; index_width < map_width; index_width += 1)
                 {
                     cute_tiled_tile_descriptor_t *tile = tileset->tiles;
-                    int gid = remove_gid_flip_bits(layer_content[tile_index]);
+                    int gid = remove_gid_flip_bits(layer_content[row_base + index_width]);
 
                     if (tile_has_properties(gid, &tile, map->handle))
                     {
                         int prop_cnt = get_tile_property_count(tile);
                         cute_tiled_property_t *props = tile->properties;
-                        tile_desc_t *current_tile = &map->tile_desc[tile_index];
+                        tile_desc_t *current_tile = &map->tile_desc[row_base + index_width];
 
-                        if (get_boolean_property(H_IS_DEADLY, props, prop_cnt, map))
+                        for (int pi = 0; pi < prop_cnt; pi += 1)
                         {
-                            current_tile->is_deadly = true;
+                            Uint64 ph = generate_hash((const unsigned char *)props[pi].name.ptr);
+                            if (ph == H_IS_DEADLY)
+                            {
+                                current_tile->is_deadly = (bool)props[pi].data.boolean;
+                            }
+                            else if (ph == H_IS_SOLID)
+                            {
+                                current_tile->is_solid = (bool)props[pi].data.boolean;
+                            }
+                            else if (ph == H_IS_WALL)
+                            {
+                                current_tile->is_wall = (bool)props[pi].data.boolean;
+                            }
+                            else if (ph == H_OFFSET_TOP)
+                            {
+                                current_tile->offset_top = props[pi].data.integer;
+                            }
                         }
-                        if (get_boolean_property(H_IS_SOLID, props, prop_cnt, map))
-                        {
-                            current_tile->is_solid = true;
-                        }
-                        if (get_boolean_property(H_IS_WALL, props, prop_cnt, map))
-                        {
-                            current_tile->is_wall = true;
-                        }
-                        current_tile->offset_top = get_integer_property(H_OFFSET_TOP, props, prop_cnt, map);
                     }
                 }
             }
@@ -987,7 +993,7 @@ bool render_map(map_t *map, SDL_Renderer *renderer, bool *has_updated, int cam_x
                 register int tilewidth = map->cached_tilewidth;
                 register int tileheight = map->cached_tileheight;
                 int *layer_content = layer->data;
-                register int tile_index = 0;
+                int unroll_end = map_width & ~3;
 
                 src.w = dst.w = tilewidth;
                 src.h = dst.h = tileheight;
@@ -995,22 +1001,28 @@ bool render_map(map_t *map, SDL_Renderer *renderer, bool *has_updated, int cam_x
                 for (int index_height = 0; index_height < map_height; index_height += 1)
                 {
                     register int dst_y = index_height * tileheight;
-                    for (int index_width = 0; index_width < map_width; index_width += 1, tile_index += 1)
+                    register int row_base = index_height * map_width;
+                    int index_width = 0;
+
+                    for (; index_width < unroll_end; index_width += 4)
                     {
-                        int gid = remove_gid_flip_bits(layer_content[tile_index]);
+                        int base = row_base + index_width;
+                        int g0 = remove_gid_flip_bits(layer_content[base]);
+                        int g1 = remove_gid_flip_bits(layer_content[base + 1]);
+                        int g2 = remove_gid_flip_bits(layer_content[base + 2]);
+                        int g3 = remove_gid_flip_bits(layer_content[base + 3]);
 
-                        if (is_gid_valid(gid, map->handle))
-                        {
-                            dst.x = index_width * tilewidth;
-                            dst.y = dst_y;
+                        dst.y = dst_y;
+                        if (g0) { dst.x = index_width * tilewidth;       get_tile_position(g0, &src.x, &src.y, map->handle); SDL_BlitSurface(map->tileset_surface, &src, map->render_canvas, &dst); }
+                        if (g1) { dst.x = (index_width + 1) * tilewidth; get_tile_position(g1, &src.x, &src.y, map->handle); SDL_BlitSurface(map->tileset_surface, &src, map->render_canvas, &dst); }
+                        if (g2) { dst.x = (index_width + 2) * tilewidth; get_tile_position(g2, &src.x, &src.y, map->handle); SDL_BlitSurface(map->tileset_surface, &src, map->render_canvas, &dst); }
+                        if (g3) { dst.x = (index_width + 3) * tilewidth; get_tile_position(g3, &src.x, &src.y, map->handle); SDL_BlitSurface(map->tileset_surface, &src, map->render_canvas, &dst); }
+                    }
 
-                            int tmp_x, tmp_y;
-                            get_tile_position(gid, &tmp_x, &tmp_y, map->handle);
-                            src.x = tmp_x;
-                            src.y = tmp_y;
-
-                            SDL_BlitSurface(map->tileset_surface, &src, map->render_canvas, &dst);
-                        }
+                    for (; index_width < map_width; index_width += 1)
+                    {
+                        int gid = remove_gid_flip_bits(layer_content[row_base + index_width]);
+                        if (gid) { dst.x = index_width * tilewidth; dst.y = dst_y; get_tile_position(gid, &src.x, &src.y, map->handle); SDL_BlitSurface(map->tileset_surface, &src, map->render_canvas, &dst); }
                     }
                 }
 
@@ -1020,32 +1032,30 @@ bool render_map(map_t *map, SDL_Renderer *renderer, bool *has_updated, int cam_x
         }
         else if (is_layer_of_type(OBJECT_GROUP, layer, map))
         {
+            cute_tiled_map_t *handle = map->handle;
+            register int tilewidth_obj  = map->cached_tilewidth;
+            register int tileheight_obj = map->cached_tileheight;
+            register int map_width_obj  = map->cached_map_width;
+            src.w = dst.w = tilewidth_obj;
+            src.h = dst.h = tileheight_obj;
             cute_tiled_object_t *object = get_head_object(layer, map);
             while (object)
             {
                 int gid = remove_gid_flip_bits(object->gid);
 
-                if (is_gid_valid(gid, map->handle))
+                if (gid)
                 {
                     int anim_length = 0;
                     int id = 0;
 
                     // Cache object position to avoid multiple pointer dereferences.
-                    float obj_x = object->x;
-                    float obj_y = object->y;
+                    dst.x = (int)object->x;
+                    dst.y = (int)object->y - tileheight_obj;
 
-                    src.w = dst.w = map->handle->tilesets->tilewidth;
-                    src.h = dst.h = map->handle->tilesets->tileheight;
-                    dst.x = (int)obj_x;
-                    dst.y = (int)obj_y - map->handle->tilesets->tileheight;
+                    get_tile_position(gid, &src.x, &src.y, handle);
 
-                    int tmp_x, tmp_y;
-                    get_tile_position(gid, &tmp_x, &tmp_y, map->handle);
-                    src.x = tmp_x;
-                    src.y = tmp_y;
-
-                    set_object_animation(gid, &anim_length, &id, map->handle);
-                    map->obj[index].gid = get_local_id(gid, map->handle);
+                    set_object_animation(gid, &anim_length, &id, handle);
+                    map->obj[index].gid = get_local_id(gid, handle);
                     map->obj[index].id = id;
                     map->obj[index].x = dst.x;
                     map->obj[index].y = dst.y;
@@ -1062,16 +1072,13 @@ bool render_map(map_t *map, SDL_Renderer *renderer, bool *has_updated, int cam_x
 
                     if (prev_layer)
                     {
-                        int index_width = dst.x / map->handle->tilesets->tilewidth;
-                        int index_height = dst.y / map->handle->tilesets->tileheight;
+                        int iw = dst.x / tilewidth_obj;
+                        int ih = dst.y / tileheight_obj;
                         int *layer_content_below = prev_layer->data;
-                        int gid_below = remove_gid_flip_bits(layer_content_below[(index_height * map->handle->width) + index_width]);
-                        if (is_gid_valid(gid_below, map->handle))
+                        int gid_below = remove_gid_flip_bits(layer_content_below[(ih * map_width_obj) + iw]);
+                        if (gid_below)
                         {
-                            int tmp_x_below, tmp_y_below;
-                            get_tile_position(gid_below, &tmp_x_below, &tmp_y_below, map->handle);
-                            map->obj[index].canvas_src_x = tmp_x_below;
-                            map->obj[index].canvas_src_y = tmp_y_below;
+                            get_tile_position(gid_below, &map->obj[index].canvas_src_x, &map->obj[index].canvas_src_y, handle);
                         }
                     }
 
@@ -1119,21 +1126,11 @@ bool object_intersects(aabb_t bb, map_t *map, int *index_ptr)
                     if (is_gid_valid(gid, map->handle))
                     {
                         aabb_t object_aabb;
-#if defined(__SYMBIAN32__)
-                        // All tiles are 16x16, use constant for half-size.
                         const int obj_half = 8;
                         object_aabb.left = object->x - obj_half;
                         object_aabb.right = object->x + obj_half;
                         object_aabb.top = object->y - obj_half;
                         object_aabb.bottom = object->y + obj_half;
-#else
-                        int obj_half_width = (int)(object->width / 2);
-                        int obj_half_height = (int)(object->height / 2);
-                        object_aabb.left = object->x - obj_half_width;
-                        object_aabb.right = object->x + obj_half_width;
-                        object_aabb.top = object->y - obj_half_height;
-                        object_aabb.bottom = object->y + obj_half_height;
-#endif
 
                         if (do_intersect(bb, object_aabb))
                         {
@@ -1157,15 +1154,7 @@ int get_tile_index(int pos_x, int pos_y, map_t *map)
     // Use cached dimensions to reduce pointer dereferences
     register int map_width = map->cached_map_width;
     register int max_index = map->tile_desc_count - 1;
-
-#if defined(__SYMBIAN32__)
-    // N-Gage optimization: Tiles are always 16x16, use bit shift by 4
     register int index = (pos_x >> 4) + ((pos_y >> 4) * map_width);
-#else
-    register int tilewidth = map->cached_tilewidth;
-    register int tileheight = map->cached_tileheight;
-    register int index = (pos_x / tilewidth) + ((pos_y / tileheight) * map_width);
-#endif
 
     return (index > max_index) ? max_index : index;
 }
